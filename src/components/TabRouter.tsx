a0e84d3ee1c7381dc2f8f1f991430128b4c7ce90
Aloompa <aloompa@Aloompas-MBP.attlocal.net>
2019-09-23 10:57:46 -0500

    Tab Router Initial

---
+++ src/components/TabRouter.tsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+import { TouchableOpacity, View } from "react-native";
+
+const TabRouter = (props: {
+  activeTabIndex: number;
+  setActiveTab: any;
+  bottomTab: boolean;
+  tabButtons: any[]
+  tabViews: any[];
+  viewHeightReduction: number;
+}) => {  
+  if(props.tabViews.length === 1){
+    return props.tabViews[0];
+  }
+
+  const tabBar = (
+    <View
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        height: 52
+      }}
+    >
+      {props.tabButtons.map((button, index) => (
+        <TouchableOpacity onPress={() => props.setActiveTab(index)}>
+          {button}
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
+
+  const tabView = (
+    <View style={{ height: `Calc(100vh - ${props.viewHeightReduction}px` }}>
+      {props.tabViews[props.activeTabIndex]}
+    </View>
+  );
+  
+  const view = [tabBar, tabView];
+
+  return <View>{props.bottomTab ? view.reverse() : view}</View>;
+};
+
+export default TabRouter;
--- src/examples/Routes.tsx
+++ src/examples/Routes.tsx
@@ -1,12 +1,29 @@
+import * as React from 'react';
+import { Image } from 'react-native';
 import Home from './routes/Home';
 import Modal from './routes/Modal';
 import Page2 from './routes/Page2';
+import Page2b from './routes/Page2b';
 import Page3 from './routes/Page3';
 import TopNav from './components/TopNav';
 import { always } from 'ramda';
 import { createRoutes } from '../index';
 
 const Routes = createRoutes({
+    tabs: [
+      {
+        button: <Image source={{uri: 'https://s3.amazonaws.com/assets.aloompa.com/TestFolder/reservations-tab-icon-browse.svg'}} style={{width: 35, height: 30, marginLeft: 18, marginTop: 8}}></Image>,
+        initial: 'Home'
+      },
+      {
+        button: <Image source={{uri:'https://s3.amazonaws.com/assets.aloompa.com/TestFolder/reservations-tab-icon-tickets.svg'}} style={{width: 30, height: 25, marginTop: 8}}/>,
+        initial: 'Page2'
+      },
+      {
+        button: <Image source={{uri:'https://s3.amazonaws.com/assets.aloompa.com/TestFolder/reservations-tab-icon-account.svg'}} style={{position: 'relative', width: 30, height: 30, marginRight: 18, marginTop: 8}}/>,
+        initial: 'Page3'
+      }
+    ],
     routes: {
         Home: {
             route: Home,
@@ -16,8 +33,13 @@ const Routes = createRoutes({
             route: Page2,
             getTitle: always('Page 2')
         },
+        Page2b: {
+            route: Page2b,
+            getTitle: always('Page 2B')
+        },
         Page3: {
-            route: Page3
+            route: Page3,
+            getTitle: always('Page 3 Home')
         },
         Modal: {
             route: Modal,
